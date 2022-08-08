cd319f4333070972809f64803edf3bfe42e025e7
sottaku-tatuki <ajurite110@gmail.com>
2022-08-08 18:50:22 +0900

    ビューファイルname属性の修正とvalidateの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
   with_options presence: true do
     validates :nickname      
-    validates :family_name   
-    validates :first_name
-    validates :family_name_kana  
-    validates :first_name_kana
     validates :birth_day  
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :family_name   
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana   
+    end
   end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
 
   has_many :items
   has_many :orders
