e3f418bbe1997b40c1ee9c89cf07ab2d11c2d821
sottaku-tatuki <ajurite110@gmail.com>
2022-08-19 19:19:29 +0900

    商品購入機能実装後に予定していたコードの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     # 出品者かつ未購入品のみedit可能になる機能は後に実装
-    if @item.user_id == current_user.id
+    if @item.user_id == current_user.id && @item.order.nil?
     else
       redirect_to root_path
     end
